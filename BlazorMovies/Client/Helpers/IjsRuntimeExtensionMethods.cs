b06f6face9cfc74e348c4fbd9c354884ce0a14e0
njeziofficial <njezichigozie@yahoo.com>
2020-07-13 18:25:52 +0100

    Running Js from C# - Helpers

---
+++ BlazorMovies/Client/Helpers/IjsRuntimeExtensionMethods.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.JSInterop;
+using System;
+using System.Threading.Tasks;
+
+namespace BlazorMovies.Client.Helpers
+{
+    public static class IjsRuntimeExtensionMethods
+    {
+        public static async ValueTask<bool> Confirm(this IJSRuntime js, string message)
+        {
+            try
+            {
+                await js.InvokeVoidAsync("console.log", "Hello from IjsRuntimeExtensionMethods");
+                if (message == null) throw new ArgumentNullException(nameof(message));
+                return await js.InvokeAsync<bool>("confirm", "message");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                throw;
+            }
+
+        }
+    }
+}
